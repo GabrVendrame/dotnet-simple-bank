6fa90811e5c1a4f0feae0f58ac18e492950763f5
GabrVendrame <gabrielvendrame7@gmail.com>
2025-04-15 15:18:31 -0300

    chore: change on configuration injection

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -9,22 +9,26 @@ namespace dotnet_simplified_bank.Services
 {
     public class TokenService(IConfiguration config) : ITokenService
     {
+        private readonly IConfiguration _config = config;
         private readonly SymmetricSecurityKey _securityKey = new(Encoding.UTF8.GetBytes(config["JWT:LoginKey"]!));
 
         private string CreateToken(User user)
         {
             if (string.IsNullOrEmpty(user.Email)) throw new ArgumentNullException(user.Email);
 
+            var role = user.CpfCnpj.Length == 11 ? "User" : "Seller";
+
             var claims = new List<Claim>
             {
                 new (ClaimTypes.Email, user.Email),
+                new (ClaimTypes.Role, role)
             };
 
             var credentials = new SigningCredentials(_securityKey, SecurityAlgorithms.HmacSha512);
 
             var descriptor = new JwtSecurityToken(
-                issuer: config["JWT:Issuer"],
-                audience: config["JWT:Audience"],
+                issuer: _config["JWT:Issuer"],
+                audience: _config["JWT:Audience"],
                 claims: claims,
                 expires: DateTime.UtcNow.AddDays(1),
                 signingCredentials: credentials
